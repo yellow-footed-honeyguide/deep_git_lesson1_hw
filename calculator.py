f791802c580951cc3aa9b275a5e0d88ea6577f27
Sergey Venetskiy <s.venetsky@gmail.com>
2025-06-17 11:57:26 +0300

    initial commit

---
+++ calculator.py
@@ -0,0 +1,27 @@
+# Calculator implementation
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    # TODO: fix this bug later
+    return a + b  # BUG: should be a - b
+
+def multiply(a, b):
+    # Not implemented
+    pass
+
+# Quick test
+print("2 + 3 =", add(2, 3))
+print("5 - 2 =", subtract(5, 2))  # This shows 7 instead of 3!
+
+# My lunch break notes - DELETE THIS
+# Had pizza today, was good
+
+def divide(a, b):
+    return a / b  # TODO: add zero check
+
+# Unfinished advanced features
+class Scientific:
+    def sqrt(self, x):
+        # TODO
+        pass
